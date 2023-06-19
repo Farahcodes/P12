ccc9d12d66e7e0d4fd4bfc1de42e470591073b39
Farah Fournat <farah.fournat@gmail.com>
2023-06-19 17:04:20 +0200

    added routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import { NavTop, NavLeft } from './Nav';
 import Dashboard from './Dashboard';
 
-ReactDOM.render(
-  <React.StrictMode>
+import './styles/index.css';
+
+/**
+ * Navigation
+ *
+ * @return {ReactElement} Return the user profile page
+ */
+const Navigation = () => (
+  <>
     <NavTop />
     <main>
       <NavLeft />
       <Dashboard />
     </main>
+  </>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user/:id" element={<Navigation />} />
+        <Route path="*" element={<Navigate to="/user/12" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
